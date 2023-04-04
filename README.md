31d1e7d9a22c8df055cd850bc36608d7e46894b0
Mewthree <goldenophanim@gmail.com>
2023-04-04 04:55:37 -0300

    Placeholder image + right click menu + resizing + settings interface

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# emetselchdesktoppet
+# WindUpEmetSelch
 I started taking my ADHD meds on a day I didn't have work.
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import json
 import tkinter as tk
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 
-f = open('settings.json')
+f = open('config.json')
 settings = json.load(f)
 for i in settings:
     print(i)
 
 window = tk.Tk()
+window.geometry(f"{settings['size']}x{settings['size']}")
 window.config(highlightbackground='black')
 window.overrideredirect(True)
 window.wm_attributes('-transparentcolor','black')
 
-image = ImageTk.PhotoImage(Image.open("assets/clockwork_solus_transparent.png"))
-tk.Label(window, image=image, bd=0, bg='black').pack()
+image = ImageTk.PhotoImage(Image.open("assets/solus1.png").resize((settings['size'],settings['size']), Image.LANCZOS))
+solus = tk.Label(window, image=image, bd=0, bg='black')
+solus.pack()
 
-window.mainloop()
+rightClickMenu = tk.Menu(window, tearoff=0)
+rightClickMenu.add_command(label="Defeat", command=window.destroy)
+
+def showRightClickMenu(event):
+    try:
+        rightClickMenu.tk_popup(event.x_root, event.y_root)
+    finally:
+        rightClickMenu.grab_release()
+
+solus.bind("<Button-3>", showRightClickMenu)
+
+window.mainloop() 
